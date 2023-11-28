024486a21c1db00e0573fa045e349875200c9474
ryanocerrous <antiheroryan@gmail.com>
2023-11-28 14:02:51 +0000

    Week One Practical Final

---
+++ Week One Work.py
@@ -0,0 +1,18 @@
+# Introduction to Programming
+
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+
+
+
+    print('the program has executed')
+print('the program has received input')
+print('the program has shut down')
+print('the program is idle')
+print('the program has shut down')
+
+help(print)
+
+quit()
+
